8774cb0587ce29e5b17d10225fa8260cd7142f4c
Vitalii <vetajika@gmail.com>
2023-02-10 16:41:44 +0200

    node server.js

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 // server.js
-let express = require('express');
-let path = require('path');
-let serveStatic = require('serve-static');
-app = express();
-app.use(serveStatic(__dirname + "/dist"));
-let port = process.env.PORT || 5000;
-let hostname = '127.0.0.1';
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
 
-app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+const app = express()
+
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+app.get(/.*/, function (req, res) {
+    res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
+
+const port = process.env.PORT || 3000
+app.listen(port)
+console.log(`listening port: ${port}`)
